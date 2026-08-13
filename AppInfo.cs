9b658fd11a2d90e70eb5e287c3f0d63f259898b1
IamN8Wright <iamn8wright@gmail.com>
2026-08-13 00:28:58 -0800

    Define InNasc product identity and website

--- AppInfo.cs
+++ AppInfo.cs
@@ -2,13 +2,17 @@ namespace AVMatrixStudio;
 
 internal static class AppInfo
 {
+    public const string ProductName = "InNasc";
+    public const string Tagline = "Systems in context.";
+    public const string Website = "https://InNasc.com";
+
     public static string Revision
     {
         get
         {
             var version = typeof(AppInfo).Assembly.GetName().Version;
             return version is null
-                ? "4.0.0"
+                ? "5.0.0"
                 : $"{version.Major}.{version.Minor}.{Math.Max(0, version.Build)}";
         }
     }
